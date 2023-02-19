e2268078d2d386e931a223b77b206c7222ce7f61
Robert Trzebinski <robert.trzebinski@urbansportsclub.com>
2023-02-19 09:38:04 +0100

    POC WIP

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
@@ -50,10 +51,101 @@ func main() {
 // embedding app.Compo into a struct.
 type hello struct {
 	app.Compo
+
+	question    string
+	answer      string
+	showAnswer  bool
+	goodAnswers int
+	badAnswers  int
 }
 
-// The Render method is where the component appearance is defined. Here, a
-// "Hello World!" is displayed as a heading.
+// init fetch random exercises, showAnswer question, hide answer
+func (h *hello) init() {
+	h.showAnswer = false
+	question, answer := exercises()
+	h.question = question
+	h.answer = answer
+}
+
+// The Render method is where the component appearance is defined.
 func (h *hello) Render() app.UI {
-	return app.H1().Text("Hello World!")
+	// will be re-run on every button click
+	if h.question == "" && h.answer == "" {
+		h.init()
+	}
+
+	return app.Div().Body(
+		app.P().Body(
+			app.Button().
+				Text("Next exercise").
+				OnClick(func(ctx app.Context, e app.Event) {
+					h.init()
+				}).
+				Style("margin-right", "10px"),
+			app.Button().
+				Text("Show Answer").
+				OnClick(func(ctx app.Context, e app.Event) {
+					h.showAnswer = true
+				}).
+				Style("margin-right", "10px"),
+			app.Button().
+				Text("Good Answer").
+				OnClick(func(ctx app.Context, e app.Event) {
+					h.goodAnswers++
+					h.init()
+				}).
+				Style("margin-right", "10px"),
+			app.Button().
+				Text("Bad Answer").
+				OnClick(func(ctx app.Context, e app.Event) {
+					h.badAnswers++
+					h.init()
+				}).
+				Style("margin-right", "10px"),
+		),
+		app.H2().Body(
+			app.Text("What is the capital of "),
+			app.If(h.question != "",
+				app.Text(h.question),
+			).Else(
+				app.Text("..."),
+			),
+			app.Text("?"),
+		),
+		app.H2().Body(
+			app.If(h.answer != "",
+				app.Text(h.answer),
+			).Else(
+				app.Text("?"),
+			),
+		).Hidden(!h.showAnswer),
+		app.P().Body(
+			app.Text("Good answers: "),
+			app.Text(h.goodAnswers),
+		),
+		app.P().Body(
+			app.Text("Bad answers: "),
+			app.Text(h.badAnswers),
+		),
+	)
+}
+
+func exercises() (string, string) {
+	capitals := map[string]string{
+		"Poland":      "Warsaw",
+		"Germany":     "Berlin",
+		"France":      "Paris",
+		"Netherlands": "Amsterdam",
+		"Spain":       "Madrid",
+	}
+
+	i := rand.Intn(len(capitals))
+	for k := range capitals {
+		if i == 0 {
+			return k, capitals[k]
+		}
+		i--
+	}
+
+	panic("never")
 }
