51d3e9f3ce7b3b29cef6f0223f4a800d37e96200
Robert Trzebinski <robert.trzebinski@urbansportsclub.com>
2023-03-17 07:38:16 +0100

    TestIncrementGoodAnswers

--- internal/storage/postgres/writer_test.go
+++ internal/storage/postgres/writer_test.go
@@ -62,3 +62,59 @@ func TestIncrementBadAnswers(t *testing.T) {
 			assert.Equal(t, 0, exerciseResult.GoodAnswers)
 		})
 }
+
+func TestIncrementGoodAnswers(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test")
+	}
+
+	ctx := context.Background()
+
+	// container and database
+	container, db, err := createPostgresContainer(ctx, "testdb")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+	defer container.Terminate(ctx)
+
+	// migration
+	mig, err := newMigrator(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = mig.Up()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := NewWriter(db)
+
+	exercise := &entities.Exercise{
+		Question: "question",
+		Answer:   "answer",
+	}
+
+	storeExercise(db, exercise)
+
+	t.Run(
+		"not existing exercise result", func(t *testing.T) {
+			w.IncrementGoodAnswers(exercise.Id)
+
+			exerciseResult := findExerciseResultByExerciseId(db, exercise.Id)
+
+			assert.Equal(t, 1, exerciseResult.GoodAnswers)
+			assert.Equal(t, 0, exerciseResult.BadAnswers)
+		})
+
+	t.Run(
+		"existing exercise result", func(t *testing.T) {
+			w.IncrementGoodAnswers(exercise.Id)
+
+			exerciseResult := findExerciseResultByExerciseId(db, exercise.Id)
+
+			assert.Equal(t, 2, exerciseResult.GoodAnswers)
+			assert.Equal(t, 0, exerciseResult.BadAnswers)
+		})
+}
