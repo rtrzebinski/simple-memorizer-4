f36f82648dbef76d2895a7214aa227067a1a6099
Robert Trzebinski <robert@trzebinski.info>
2023-03-18 10:08:33 +0100

    refactor

--- internal/frontend/api_client.go
+++ internal/frontend/api_client.go
@@ -22,28 +22,6 @@ func NewApiClient(client HttpClient, host string, scheme string) *ApiClient {
 	return &ApiClient{client: client, host: host, scheme: scheme}
 }
 
-func (c *ApiClient) performRequestTo(method, url string, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %w", err)
-	}
-
-	req.Header.Add("content-type", "application/json")
-
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to call API endpoint: %w", err)
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		payload, _ := io.ReadAll(resp.Body)
-
-		return nil, fmt.Errorf("API returned with status code '%d': %w", resp.StatusCode, errors.New(string(payload)))
-	}
-
-	return resp, nil
-}
-
 func (c *ApiClient) FetchRandomExercise() models.Exercise {
 	resp, err := c.performRequestTo("GET", c.scheme+"://"+c.host+backend.FetchRandomExercise, nil)
 	if err != nil {
@@ -100,4 +78,26 @@ func (c *ApiClient) IncrementGoodAnswers(exerciseId int) {
 	}
 
 	defer resp.Body.Close()
+}
+
+func (c *ApiClient) performRequestTo(method, url string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header.Add("content-type", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to call API endpoint: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		payload, _ := io.ReadAll(resp.Body)
+
+		return nil, fmt.Errorf("API returned with status code '%d': %w", resp.StatusCode, errors.New(string(payload)))
+	}
+
+	return resp, nil
 }
