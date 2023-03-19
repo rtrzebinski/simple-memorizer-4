42519325fad2948e68b7543c6f8eaea8113f4f60
Robert Trzebinski <robert@trzebinski.info>
2023-03-19 11:02:40 +0100

    finish api client tests

--- internal/frontend/api_client_test.go
+++ internal/frontend/api_client_test.go
@@ -3,6 +3,8 @@ package frontend
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/rtrzebinski/simple-memorizer-4/internal/backend"
+	"github.com/rtrzebinski/simple-memorizer-4/internal/backend/routes"
 	"github.com/rtrzebinski/simple-memorizer-4/internal/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -10,6 +12,11 @@ import (
 	"io"
 	"net/http"
 	"testing"
+)
+
+var (
+	host   = "example.com"
+	scheme = "http"
 )
 
 type ApiClientSuite struct {
@@ -21,7 +28,7 @@ type ApiClientSuite struct {
 
 func (suite *ApiClientSuite) SetupTest() {
 	suite.httpClientMock = new(HttpClientMock)
-	suite.apiClient = NewApiClient(suite.httpClientMock, "example.com", "http")
+	suite.apiClient = NewApiClient(suite.httpClientMock, host, scheme)
 }
 
 func TestSuite(t *testing.T) {
@@ -43,6 +50,11 @@ func (suite *ApiClientSuite) TestFetchRandomExercise() {
 	}
 
 	suite.httpClientMock.On("Do", mock.MatchedBy(func(req *http.Request) bool {
+		suite.Equal("GET", req.Method)
+		suite.Equal(backend.FetchRandomExercise, req.URL.RequestURI())
+		suite.Equal(host, req.URL.Host)
+		suite.Equal(scheme, req.URL.Scheme)
+
 		return true
 	})).Return(&http.Response{
 		StatusCode: http.StatusOK,
@@ -56,9 +68,49 @@ func (suite *ApiClientSuite) TestFetchRandomExercise() {
 }
 
 func (suite *ApiClientSuite) TestIncrementBadAnswers() {
+	exerciseId := 123
 
+	suite.httpClientMock.On("Do", mock.MatchedBy(func(req *http.Request) bool {
+		suite.Equal("POST", req.Method)
+		suite.Equal(backend.IncrementBadAnswers, req.URL.RequestURI())
+		suite.Equal(host, req.URL.Host)
+		suite.Equal(scheme, req.URL.Scheme)
+
+		var input routes.IncrementBadAnswersReq
+		err := json.NewDecoder(req.Body).Decode(&input)
+		suite.NoError(err)
+		suite.Equal(exerciseId, input.ExerciseId)
+
+		return true
+	})).Return(&http.Response{
+		StatusCode: http.StatusOK,
+		Body:       io.NopCloser(bytes.NewReader([]byte{})),
+	}, nil)
+
+	err := suite.apiClient.IncrementBadAnswers(exerciseId)
+	assert.NoError(suite.T(), err)
 }
 
 func (suite *ApiClientSuite) TestIncrementGoodAnswers() {
+	exerciseId := 456
 
+	suite.httpClientMock.On("Do", mock.MatchedBy(func(req *http.Request) bool {
+		suite.Equal("POST", req.Method)
+		suite.Equal(backend.IncrementGoodAnswers, req.URL.RequestURI())
+		suite.Equal(host, req.URL.Host)
+		suite.Equal(scheme, req.URL.Scheme)
+
+		var input routes.IncrementGoodAnswersReq
+		err := json.NewDecoder(req.Body).Decode(&input)
+		suite.NoError(err)
+		suite.Equal(exerciseId, input.ExerciseId)
+
+		return true
+	})).Return(&http.Response{
+		StatusCode: http.StatusOK,
+		Body:       io.NopCloser(bytes.NewReader([]byte{})),
+	}, nil)
+
+	err := suite.apiClient.IncrementGoodAnswers(exerciseId)
+	assert.NoError(suite.T(), err)
 }
