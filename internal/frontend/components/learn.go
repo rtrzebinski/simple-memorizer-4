b692a0deca090d0bcc0027b1c1874ed27a968290
Robert Trzebinski <robert@trzebinski.info>
2023-05-28 14:52:09 +0200

    bugfix: bind keys and swipes only once

--- internal/frontend/components/learn.go
+++ internal/frontend/components/learn.go
@@ -8,9 +8,11 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"sync"
 )
 
 var pathLearn = "/learn"
+var once sync.Once
 
 // A Learn component
 type Learn struct {
@@ -49,8 +51,10 @@ func (c *Learn) OnMount(ctx app.Context) {
 
 	c.api = frontend.NewApiClient(&http.Client{}, u.Host, u.Scheme)
 	c.handleNextExercise()
-	c.bindKeys()
-	c.bindSwipes()
+	once.Do(func() {
+		c.bindKeys()
+		c.bindSwipes()
+	})
 }
 
 // The Render method is where the component appearance is defined.
