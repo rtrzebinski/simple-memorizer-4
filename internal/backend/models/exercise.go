8df08edfdef4f8c19c988931b59b9e7248b17df3
Robert Trzebinski <robert@trzebinski.info>
2024-10-19 16:47:27 +0200

    refactor

--- internal/backend/models/exercise.go
+++ internal/backend/models/exercise.go
@@ -1,10 +1,9 @@
 package models
 
 type Exercise struct {
-	Id                int
-	Lesson            *Lesson
-	Question          string
-	Answer            string
-	Results           Results
-	ResultsProjection ResultsProjection
+	Id       int
+	Lesson   *Lesson
+	Question string
+	Answer   string
+	Results  Results
 }
