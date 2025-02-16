643ae1a7d9eb9348802634415719cda92f774d69
Robert Trzebinski <robert@trzebinski.info>
2025-02-16 16:50:51 +0100

    auth service test

--- internal/services/auth/grpc/mocks.go
+++ internal/services/auth/grpc/mocks.go
@@ -11,12 +11,10 @@ type ServiceMock struct {
 
 func (m *ServiceMock) Register(ctx context.Context, name, email, password string) (authToken string, err error) {
 	called := m.Called(ctx, name, email, password)
-
 	return called.Get(0).(string), called.Error(1)
 }
 
 func (m *ServiceMock) SignIn(ctx context.Context, email, password string) (authToken string, err error) {
 	called := m.Called(ctx, email, password)
-
 	return called.Get(0).(string), called.Error(1)
 }
