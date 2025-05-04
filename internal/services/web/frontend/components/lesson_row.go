739b55802152f43dcbe38e7563261290f8c2d858
Robert Trzebinski <robert@trzebinski.info>
2025-05-04 13:55:34 +0200

    hidde lesson description from the UI

--- internal/services/web/frontend/components/lesson_row.go
+++ internal/services/web/frontend/components/lesson_row.go
@@ -27,9 +27,9 @@ func (compo *LessonRow) Render() app.UI {
 		app.Td().Style("border", "1px solid black").Body(
 			app.Text(compo.lesson.Name),
 		),
-		app.Td().Style("border", "1px solid black").Body(
-			app.Text(compo.lesson.Description),
-		),
+		//app.Td().Style("border", "1px solid black").Body(
+		//	app.Text(compo.lesson.Description),
+		//),
 		app.Td().Style("border", "1px solid black").Body(
 			app.Text(compo.lesson.ExerciseCount),
 		),
